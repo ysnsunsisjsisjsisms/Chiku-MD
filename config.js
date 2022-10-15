f1459edc0eced992b1bc18a9c2c3f83671fec6f9
Ayush-pandey-u <113597722+Ayush-pandey-u@users.noreply.github.com>
2022-10-15 05:54:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ global.sessionName = "session"
 
 // Prefix //
 // 🎗don't change the prefix if you can change the prefix may be bot doesn't work properly//
-global.prefa = ['-'] 
+global.prefa = ['.'] 
 
 
 global.location = "bihar, India"
